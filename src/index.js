fa4b83129a7f2c1a0b09462151a6e380118af291
AlexRudnyk <alexander.rudnyk@ukr.net>
2023-02-23 15:01:47 +0200

    fixes package.json and base url in index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/My-phonebook-front-back/">
+        <BrowserRouter basename="/Phonebook-front-back/">
           <App />
         </BrowserRouter>
       </PersistGate>
